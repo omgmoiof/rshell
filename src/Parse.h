f9e021350c7430380972b7510760735f8a6e37c3
omgmoiof <pwill008@ucr.edu>
2017-05-11 00:00:46 -0700

    Update Parse.h

--- src/Parse.h
+++ src/Parse.h
@@ -1,1 +1,31 @@
+#ifndef PARSE_H
+#define PARSE_H
 
+
+#include "Runcmd.h"
+#include "And.h"
+#include "Or.h"
+#include "Semicolon.h"
+#include "Command.h"
+#include "Connector.h"
+#include "Execvpcmd.h"
+#include "Exit.h"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <pwd.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+class Parse
+{
+    public:
+        Parse();
+        void run();
+        Runcmd* parse(std::string&);
+    
+};
+
+#endif
