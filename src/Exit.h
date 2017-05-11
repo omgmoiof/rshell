d597cb7060c79cebe9766453bbaf69fa0f0cd2ca
omgmoiof <pwill008@ucr.edu>
2017-05-11 00:18:12 -0700

    Update Exit.h

--- src/Exit.h
+++ src/Exit.h
@@ -10,16 +10,4 @@ class Exit : public Command
     
     
 };
-#endif#ifndef EXIT_H
-#define EXIT_H
-#include "Command.h"
-
-class Exit : public Command
-{
-    public:
-        Exit();
-        bool run();
-    
-    
-};
 #endif
