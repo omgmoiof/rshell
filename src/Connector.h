ab6e9f8e0095f2c19d50b77da27b7d870294a3a7
omgmoiof <pwill008@ucr.edu>
2017-05-10 23:56:25 -0700

    Create Connector.h

---
+++ src/Connector.h
@@ -0,0 +1,22 @@
+#ifndef CONNECTOR_H
+#define CONNECTOR_H
+
+#include "Runcmd.h"
+
+class Connector : public Runcmd
+{
+    protected:
+        Runcmd* rhs;
+        Runcmd* lhs;
+    
+    public:
+        Connector(std::string, Runcmd*, Runcmd*);
+        ~Connector();
+        virtual bool run() = 0;
+    
+    private:
+        std::string literal;
+    
+};
+
+#endif
