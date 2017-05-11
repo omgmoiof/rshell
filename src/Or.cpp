88b4d88f056b4418740c1ebd245b076166b87eee
omgmoiof <pwill008@ucr.edu>
2017-05-10 23:59:54 -0700

    Update Or.cpp

--- src/Or.cpp
+++ src/Or.cpp
@@ -1,1 +1,20 @@
 
+#include "Or.h"
+
+
+    Or::Or(Runcmd* r, Runcmd* l) : Connector("||",r,l)
+    {
+        
+        
+    }
+    
+    bool Or::run()
+    {
+        if (lhs == NULL)
+        {
+            if (rhs != NULL) return rhs->run();
+        }
+        else if (rhs == NULL) return lhs->run();
+        return lhs->run() || rhs->run();
+        
+    }
