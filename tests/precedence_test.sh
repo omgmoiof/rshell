6b4ce1f4b185f48780610d9833b3b691f1fb9621
omgmoiof <pwill008@ucr.edu>
2017-05-31 18:01:00 -0700

    Update precedence_test.sh

--- tests/precedence_test.sh
+++ tests/precedence_test.sh
@@ -1,1 +1,22 @@
 (echo a && echo b)
+(echo a && echo b) && (echo c && echo d)
+(test -e pathname && echo true)
+(test -e pathname && echo true) && echo hi
+(test -e pathname && echo true) && (echo A && echo B)
+(echo a)
+(echo a) && (echo a)
+(echo a) && (echo a) && (echo a) 
+(echo a) && (echo a) && (echo a) && (echo a)
+(echo a && echo b) || (echo c && echo d)
+(echo a && echo b) || giberish
+(echo a && echo b) || (echo a && echo hasdfjlaksjdfkjsdaf)
+(echo a && echo b) || (echo a)
+(echo a); pwd
+(echo a);(echo a);
+(echo a) #commeting
+(echo a);(echo a);(echo a);
+(echo a);(echo a);(echo a);(echo a);
+(echo a);           (echo a);
+(echo a);     (echo a);
+(echo a && echo b); echo a
+(echo a && echo b);(echo a && echo b);(echo a && echo b)
