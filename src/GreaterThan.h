4eb17e708d15719411e849375fe04ab47a5e5694
kevinle90 <kevinle90@gmail.com>
2017-06-12 15:19:13 -0700

    Create GreaterThan.h

--- src/GreaterThan.h
+++ src/GreaterThan.h
@@ -1,1 +1,14 @@
+#ifndef GreaterThan_H
+#define GreaterThan_H
 
+#include "Connector.h"
+
+class GreaterThan : public Connector
+{
+    public:
+        GreaterThan(Runcmd*,Runcmd*);
+        bool run();
+    
+};
+
+#endif
