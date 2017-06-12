4ba0e049553d3673ea4ac21f025fc77dab30353c
omgmoiof <pwill008@ucr.edu>
2017-06-12 15:21:58 -0700

    Update Stick.h

--- src/Stick.h
+++ src/Stick.h
@@ -1,1 +1,14 @@
+#ifndef Stick_H
+#define Stick_H
 
+#include "Connector.h"
+
+class Stick : public Connector
+{
+    public:
+        Stick(Runcmd*,Runcmd*);
+        bool run();
+    
+};
+
+#endif
