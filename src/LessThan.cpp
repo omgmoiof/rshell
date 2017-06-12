81cfcc82bb1e48c8181c0c2bdeb4c91e7aea0bf9
omgmoiof <pwill008@ucr.edu>
2017-06-12 15:20:27 -0700

    Update LessThan.cpp

--- src/LessThan.cpp
+++ src/LessThan.cpp
@@ -1,1 +1,38 @@
+#include "LessThan.h"
 
+
+    LessThan::LessThan(Runcmd* r, Runcmd* l) : Connector("<",r,l)
+    {
+        
+        
+    }
+    
+    bool LessThan::run()
+    {
+        char file[256];
+			strcpy(file, rhs->getCmd().c_str());
+            int redir;
+			if((redir = open(file, O_RDONLY )) == -1) 
+			{
+				perror("Input Redirection Error: Could not open file");
+				return false;
+			}
+			
+			int saveSTDin = dup(0);
+			if(-1 == dup2(redir, 0))
+            {
+	        	perror("There was an error with dup2");
+		    
+            }
+			
+			
+			lhs->run();
+			
+			if(-1 == dup2(saveSTDin, 0))
+            {
+	        	perror("There was an error with dup2");
+		    
+            }
+            return true;
+        
+    }
