c66a8a12ea45a6b819a41c26acc43a6ad9c123ef
kevinle90 <kevinle90@gmail.com>
2017-06-12 15:16:07 -0700

    Create pipe_test.sh

--- tests/pipe_test.sh
+++ tests/pipe_test.sh
@@ -1,1 +1,29 @@
 
+touch hi.txt
+echo a >> hi.txt
+echo b >> hi.txt
+echo c >> hi.txt
+echo d >> hi.txt
+echo e >> hi.txt
+echo f >> hi.txt
+echo g >> hi.txt
+echo h >> hi.txt
+echo i >> hi.txt
+echo j >> hi.txt
+echo k >> hi.txt
+echo l >> hi.txt
+echo m >> hi.txt
+echo n >> hi.txt
+echo o >> hi.txt
+echo p >> hi.txt
+echo q >> hi.txt
+echo r >> hi.txt
+echo s >> hi.txt
+echo t >> hi.txt
+echo u >> hi.txt
+echo v >> hi.txt
+echo w >> hi.txt
+echo x >> hi.txt
+echo y >> hi.txt
+echo z >> hi.txt
+
