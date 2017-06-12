1919a490447eaf5fc3c627612c854781ea819df6
kevinle90 <kevinle90@gmail.com>
2017-06-12 15:18:18 -0700

    Create DoubleGreaterThan.cpp

--- src/DoubleGreaterThan.cpp
+++ src/DoubleGreaterThan.cpp
@@ -1,1 +1,41 @@
+#include "DoubleGreaterThan.h"
 
+
+    DoubleGreaterThan::DoubleGreaterThan(Runcmd* r, Runcmd* l) : Connector(">>",r,l)
+    {
+        
+        
+    }
+    
+    bool DoubleGreaterThan::run()
+    {
+
+            char file[256];
+            strcpy(file, rhs->getCmd().c_str());
+            int redir;
+            if((redir = open(file, O_RDWR | O_APPEND | O_CREAT , S_IRUSR | S_IWUSR | S_IROTH | S_IRGRP )) == -1)
+            {
+    				perror("Error with output redirection"); //error message with output redirection
+    				return false;
+
+    		}
+    		
+            int saveStdout = dup(1);
+
+            if(-1 == dup2(redir, 1))
+            {
+	        	perror("There was an error with dup2");
+		    
+            }
+            lhs->run();
+
+            
+            if(-1 == dup2(saveStdout, 1))
+            {
+	        	perror("There was an error with dup2");
+		    
+            }
+            
+            return true;
+        
+    }
