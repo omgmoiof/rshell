8e2593cb6b79404e6d2fdaa2564e4a6f2a903597
omgmoiof <pwill008@ucr.edu>
2017-06-13 12:57:12 -0700

    Create output_redirect_test.sh

--- tests/output_redirect_test.sh
+++ tests/output_redirect_test.sh
@@ -1,1 +1,26 @@
+echo ls > hi.txt
+
+ls > hi.txt
+ls > hi.txt #comment
+ls > hi.txt # this is a comment
+
+hi.txt > hi.txt
+hi.txt > hi.txt > hi.txt
+hi.txt > hi.txt > hi.txt > hi.txt
+hi.txt > hi.txt > hi.txt > hi.txt > hi.txt
+
+ls>hi.txt
+ls  >hi.txt
+ls  > hi.txt
+ls  >  hi.txt
+ls>  hi.txt
+ls >  hi.txt
+         ls > hi.txt
+         ls> hi.txt
+         ls>hi.txt
+
+> hi.txt
+hi.txt >
+        hi.txt > hi.txt
+hi.txt               >hi.txt
 
