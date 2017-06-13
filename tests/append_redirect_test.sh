c2f79768e58c34dfb166c8f0a32b0bd3a9d2937e
omgmoiof <pwill008@ucr.edu>
2017-06-13 12:57:51 -0700

    Create append_redirect_test.sh

--- tests/append_redirect_test.sh
+++ tests/append_redirect_test.sh
@@ -1,12 +1,11 @@
-echo ls > hi.txt
+
+>>
+
 echo hello >> hi.txt
 echo green >> hi.txt
 echo blahblah >> hi.txt
 ls >> hi.txt
 
-ls > hi.txt
-ls > hi.txt #comment
-ls > hi.txt # this is a comment
 
 pwd >> hi.txt
 pwd >> hi.txt #comment
@@ -14,10 +13,6 @@ pwd >> hi.txt #this is a comment
 pwd >> hi.txt                   #comment
 pwd >> hi.txt                      #this is a comment
 
-hi.txt > hi.txt
-hi.txt > hi.txt > hi.txt
-hi.txt > hi.txt > hi.txt > hi.txt
-hi.txt > hi.txt > hi.txt > hi.txt > hi.txt
 
 hi.txt >> hi.txt
 hi.txt >> hi.txt >> hi.txt
@@ -25,17 +20,17 @@ hi.txt >> hi.txt >> hi.txt >> hi.txt
 hi.txt >> hi.txt >> hi.txt >> hi.txt >> hi.txt
 hi.txt >> hi.txt >> hi.txt >> hi.txt >> hi.txt >> hi.txt
 
-ls>hi.txt
-ls  >hi.txt
-ls  > hi.txt
-ls  >  hi.txt
-ls>  hi.txt
-ls >  hi.txt
-
-
 ls>>hi.txt
 ls  >>hi.txt
 ls  >> hi.txt
 ls  >>  hi.txt
 ls>>  hi.txt
 ls >>  hi.txt
+         ls >> hi.txt
+         ls>> hi.txt
+         ls>>hi.txt
+
+>> hi.txt
+hi.txt >>
+        hi.txt >> hi.txt
+hi.txt               >>hi.txt
