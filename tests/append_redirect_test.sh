73c712f3a4254e67be66645f4fc6bc8b152fb126
omgmoiof <pwill008@ucr.edu>
2017-06-12 18:16:14 -0700

    Update append_redirect_test.sh

--- tests/append_redirect_test.sh
+++ tests/append_redirect_test.sh
@@ -1,1 +1,41 @@
+echo ls > hi.txt
+echo hello >> hi.txt
+echo green >> hi.txt
+echo blahblah >> hi.txt
+ls >> hi.txt
 
+ls > hi.txt
+ls > hi.txt #comment
+ls > hi.txt # this is a comment
+
+pwd >> hi.txt
+pwd >> hi.txt #comment
+pwd >> hi.txt #this is a comment
+pwd >> hi.txt                   #comment
+pwd >> hi.txt                      #this is a comment
+
+hi.txt > hi.txt
+hi.txt > hi.txt > hi.txt
+hi.txt > hi.txt > hi.txt > hi.txt
+hi.txt > hi.txt > hi.txt > hi.txt > hi.txt
+
+hi.txt >> hi.txt
+hi.txt >> hi.txt >> hi.txt
+hi.txt >> hi.txt >> hi.txt >> hi.txt
+hi.txt >> hi.txt >> hi.txt >> hi.txt >> hi.txt
+hi.txt >> hi.txt >> hi.txt >> hi.txt >> hi.txt >> hi.txt
+
+ls>hi.txt
+ls  >hi.txt
+ls  > hi.txt
+ls  >  hi.txt
+ls>  hi.txt
+ls >  hi.txt
+
+
+ls>>hi.txt
+ls  >>hi.txt
+ls  >> hi.txt
+ls  >>  hi.txt
+ls>>  hi.txt
+ls >>  hi.txt
