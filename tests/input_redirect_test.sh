5a77afbd4b66248f03cebfebc5b2ace5d3a4e916
omgmoiof <pwill008@ucr.edu>
2017-06-13 13:06:20 -0700

    Create input_redirect_test.sh

--- tests/input_redirect_test.sh
+++ tests/input_redirect_test.sh
@@ -1,1 +1,15 @@
+<
+    <
 
+sort < hi.txt
+sort < hi.txt #comment
+sort < hi.txt          #comment
+sort <
+sort<
+
+       sort < hi.txt
+sort     <hi.txt
+sort     < hi.txt
+sort <     hi.txt
+sort<hi.txt
+sort <hi.txt
