1238d8b2ccfb40cc7fd840bbc928272dbfa4c4ee
omgmoiof <pwill008@ucr.edu>
2017-06-13 13:16:23 -0700

    Added test cases

--- tests/pipe_test.sh
+++ tests/pipe_test.sh
@@ -1,3 +1,27 @@
+|
+   |
+   
+ls | sort
+ls| sort
+ls|sort
+ls |sort
 
+ls > hi.txt;
+
+ls > hi.txt; hi.txt | sort
+ls > hi.txt      | sort
+ls >hi.txt | sort
+ls > hi.txt |sort
+ls>hi.txt | sort
+
+cat hi.txt | grep src
+cat hi.txt |           grep src
+cat hi.txt            | grep src
+cat hi.txt           |            grep src
+cat hi.txt|grep src
+cat hi.txt |grep src
+cat hi.txt| grep src
+
+cat < existingInputFile | tr A-Z a-z | tee newOutputFile1 | tr a-z A-Z > newOutputFile2
 
 
